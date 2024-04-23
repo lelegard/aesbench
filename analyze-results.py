372a6afff8d4e164004bac3f9272d05687593352
Thierry Lelegard <thierry@lelegard.fr>
2024-04-23 17:10:33 +0200

    Added ranks of CPUs in results

--- analyze-results.py
+++ analyze-results.py
@@ -3,9 +3,10 @@
 # aesbench - Copyright (c) 2024, Thierry Lelegard
 # BSD 2-Clause License, see LICENSE file.
 # Analyze results files and produce an analysis in markdown format.
+# With option --pprint, print the data structure.
 #----------------------------------------------------------------------------
 
-import os, sys, string, pprint
+import os, sys, pprint
 GIGA = 1000000000.0
 
 # List of CPU cores and corresponding result files.
@@ -25,8 +26,10 @@ results = [
 rootdir = os.path.dirname(os.path.abspath(sys.argv[0]))
 algos = []
 for res in results:
+    res['header'] = '%s<br/>%.2f GHz' % (res['cpu'], res['frequency'])
+    res['width'] = len(res['header'])
+    res['data'] = {}
     with open(rootdir + '/results/' + res['file'], 'r') as input:
-        res['data'] = {}
         algo = None
         for line in input:
             line = line.strip().split(':')
@@ -37,32 +40,41 @@ for res in results:
                         algo = 'AES-' + algo[6:]
                     if algo not in algos:
                         algos += [algo]
-                    res['data'][algo] = {'encrypt': 0, 'decrypt': 0}
-                elif line[0] == 'encrypt-bitrate' and algo is not None:
-                    res['data'][algo]['encrypt'] = int(line[1])
-                elif line[0] == 'decrypt-bitrate' and algo is not None:
-                    res['data'][algo]['decrypt'] = int(line[1])
+                    res['data'][algo] = {
+                        'encrypt': {'bitrate': {'value': 0, 'rank': 0}, 'bitcycle': {'value': 0.0, 'rank': 0}},
+                        'decrypt': {'bitrate': {'value': 0, 'rank': 0}, 'bitcycle': {'value': 0.0, 'rank': 0}}
+                    }
+                elif (line[0] == 'encrypt-bitrate' or line[0] == 'decrypt-bitrate') and algo is not None:
+                    op = line[0].split('-')[0]
+                    res['data'][algo][op]['bitrate']['value'] = int(line[1])
+                    res['data'][algo][op]['bitcycle']['value'] = float(line[1]) / (res['frequency'] * GIGA)
+
+# Build CPU rankings for each operation.
+for algo in algos:
+    for op in ['encrypt', 'decrypt']:
+        for value in ['bitrate', 'bitcycle']:
+            dlist = [(res['cpu'], res['data'][algo][op][value]['value']) for res in results]
+            dlist.sort(key=lambda x: x[1], reverse=True)
+            for rank in range(len(dlist)):
+                res = next(r for r in results if r['cpu'] == dlist[rank][0])
+                res['data'][algo][op][value]['rank'] = rank + 1
 
 # Generate a markdown table.
 # Results are either bits/second or bits/cycle.
-def generate_table(gen_bitrate):
+def generate_table(value_name):
     # Compute width of first column.
-    width_0 = 0
+    header_0 = 'CPU core<br/>Frequency'
+    width_0 = len(header_0)
     for name in algos:
-        width_0 = max(width_0, len(name))
-    width_0 += len(' encrypt')
-    # Compute width of each processor column.
-    for res in results:
-        res['header'] = '%s<br/>%.2f GHz' % (res['cpu'], res['frequency'])
-        res['width'] = len(res['header'])
+        width_0 = max(width_0, len(name + ' encrypt'))
     # Output headers lines.
-    print('| %*s |' % (-width_0, 'AES mode'), end='')
+    print('| %*s |' % (-width_0, header_0), end='')
     for res in results:
         print(' %s |' % (res['header']), end='')
     print('')
     print('| %s |' % (width_0 * '-'), end='')
     for res in results:
-        print(' %s: |' % ((res['width'] - 1) * '-'), end='')
+        print(' :%s: |' % ((res['width'] - 2) * '-'), end='')
     print('')
     # Output one line per AES operation.
     for algo in algos:
@@ -70,34 +82,37 @@ def generate_table(gen_bitrate):
             print('| %*s |' % (-width_0, algo + ' ' + op), end='')
             for res in results:
                 if algo in res['data'] and op in res['data'][algo]:
-                    value = res['data'][algo][op]
+                    value = res['data'][algo][op][value_name]
                 else:
-                    value = 0
-                if value <= 0 or (not gen_bitrate and res['frequency'] <= 0.0):
-                    print(' %*s |' % (res['width'], ''), end='')
-                elif gen_bitrate:
-                    print(' %*.3f |' % (res['width'], float(value) / GIGA), end='')
+                    value = {'value': 0, 'rank': 0}
+                if value['value'] <= 0:
+                    s = ''
+                elif type(value['value']) is int:
+                    s = '%.3f (%d)' % (float(value['value']) / GIGA, value['rank'])
+                elif value['value'] >= 10.0:
+                    s = '%.1f (%d)' % (value['value'], value['rank'])
+                elif value['value'] >= 1.0:
+                    s = '%.2f (%d)' % (value['value'], value['rank'])
                 else:
-                    rate = float(value) / (res['frequency'] * GIGA)
-                    if rate >= 10.0:
-                        s = '%.1f' % rate
-                    elif rate >= 1.0:
-                        s = '%.2f' % rate
-                    else:
-                        s = '%.3f' % rate
-                    print(' %*s |' % (res['width'], s), end='')
+                    s = '%.3f (%d)' % (value['value'], value['rank'])
+                print(' %*s |' % (res['width'], s), end='')
             print('')
 
 # Generate the final markdown file.
-print('# Results comparison')
-print('')
-print('## Encryption / decryption bitrate')
-print('The encryption and decryption bitrates are in gigabits per second.')
-print('')
-generate_table(True)
-print('')
-print('## Encrypted / decrypted bits per processor cycle')
-print('The values are the numbers of processed bits per cycle.')
-print('These values give a relative value of each CPU core, independently of the frequency.')
-print('')
-generate_table(False)
+if len(sys.argv) > 1 and sys.argv[1] == "--pprint":
+    pprint.pprint(results, width=132)
+else:
+    print('# Results comparison')
+    print('')
+    print('## Encryption / decryption bitrate')
+    print('The encryption and decryption bitrates are in gigabits per second.')
+    print('The number between parentheses is the rank of the CPU in the line.')
+    print('')
+    generate_table('bitrate')
+    print('')
+    print('## Encrypted / decrypted bits per processor cycle')
+    print('The values are the numbers of processed bits per cycle.')
+    print('These values give a relative value of each CPU core, independently of the frequency.')
+    print('The number between parentheses is the rank of the CPU in the line.')
+    print('')
+    generate_table('bitcycle')
